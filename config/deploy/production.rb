da2eb9db2a748baea0709d2bcc93802906bd5cf4
Alongkorn <Slurpe3@gmail.com>
2017-10-02 23:15:24 +0700

    ss

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,9 +7,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-role :app, %w{slurpe@128.199.172.14}
-role :web, %w{slurpe@128.199.172.14}
-role :db,  %w{slurpe@128.199.172.14}
+role :app, %w{slurpe3@128.199.172.14}
+role :web, %w{slurpe3@128.199.172.14}
+role :db,  %w{slurpe3@128.199.172.14}
 
 # role-based syntax
 # ==================
