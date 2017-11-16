63077e314dc0106decd5c3b7f9ee2c85a6e458a2
Alongkorn <Slurpe3@gmail.com>
2017-11-17 05:25:24 +0700

    add history

---
+++ app/views/histories/_history.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! history, :id, :category, :created_at, :updated_at
+json.url history_url(history, format: :json)
---
+++ db/migrate/20171116222346_create_histories.rb
@@ -0,0 +1,9 @@
+class CreateHistories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :histories do |t|
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
